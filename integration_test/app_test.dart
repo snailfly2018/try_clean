09e6b599f604d814c13695b66b7a72f141c07d3c
coder_asus <taobo2018@gmail.com>
2021-12-09 11:46:16 -0800

    integeration test when input 0

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:try_clean/main.dart' as app;
+import 'package:try_clean/presentation/pages/number_trivia_page.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -10,7 +12,18 @@ void main() {
       app.main();
       await tester.pumpAndSettle();
 
-      expect(find.text('Number Trivia'), findsOneWidget);
+      expect(find.byType(NumberTriviaPage), findsOneWidget);
+      expect(find.text('Start searching!'), findsOneWidget);
+
+      var textinput = find.byType(TextField);
+      expect(textinput, findsOneWidget);
+      await tester.enterText(textinput, '0');
+      await tester.pumpAndSettle();
+      await tester.tap(find.text('Search'));
+      await tester.pumpAndSettle();
+      await tester.pump(const Duration(seconds: 1));
+
+      expect(find.text('must be large than zero'), findsOneWidget);
     });
   });
 }
